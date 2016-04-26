f24790787867e45098103b81c9e216ecbed6f59f
Jeremy Brennan <jeremb@gmail.com>
2016-04-26 12:06:50 -0700

    updated scene, playerhome prefab, player home damage script

--- Assets/_CompletedAssets/Scripts/PlayerHome.cs
+++ Assets/_CompletedAssets/Scripts/PlayerHome.cs
@@ -5,19 +5,34 @@ using UnityEngine.UI;
 public class PlayerHome : MonoBehaviour {
 	public int homeHealth = 500;
 	public int currentHomeHealth;
+	public Image damageImage;
 	public Slider homeHealthSlider;  // Home health slider for the UI
+	public float flashSpeed = 5f;
+	public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+
+	bool damaged;
 
 	void Start () {
 		currentHomeHealth = homeHealth;
 	}
 	
 	// Update is called once per frame
-	void Update () {	
+	void Update () {
+		if(damaged)
+		{
+			damageImage.color = flashColour;
+		}
+		else
+		{
+			damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+		}
+		damaged = false;	
 	}
 
 	public void DamageHome(int loss) {
 		currentHomeHealth -= loss;
 		homeHealthSlider.value = currentHomeHealth;
+		damaged = true;
 
 		if (currentHomeHealth <= 0) {
 			// GAMEOVER
