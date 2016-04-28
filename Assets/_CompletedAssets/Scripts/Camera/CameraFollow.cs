d665c0c21f8bd1bb774cedc0e8bc9e6811d12b5d
Alexander Owen <owen.alex47@yahoo.com>
2016-04-28 12:58:39 -0700

    Camera works on key press in build phase

--- Assets/_CompletedAssets/Scripts/Camera/CameraFollow.cs
+++ Assets/_CompletedAssets/Scripts/Camera/CameraFollow.cs
@@ -7,20 +7,24 @@ namespace CompleteProject
     {
         public Transform target;            // The position that that camera will be following.
         public float smoothing = 5f;        // The speed with which the camera will be following.
-
+		public PhaseManager phaseManager;
 
         Vector3 offset;                     // The initial offset from the target.
 
-
-        void Start ()
-        {
+        void Start () {
             // Calculate the initial offset.
             offset = transform.position - target.position;
+
+			phaseManager = GameObject.Find ("HUDCanvas").GetComponent<PhaseManager> ();
         }
 
+        void FixedUpdate () {
+			if (phaseManager.IsBuildPhase) {
+				transform.Translate(new Vector3(Input.GetAxis("Horizontal") * 6.5f * Time.deltaTime,
+											Input.GetAxis("Vertical") * 6.5f * Time.deltaTime, 0.0f));
+				return;
+			}
 
-        void FixedUpdate ()
-        {
             // Create a postion the camera is aiming for based on the offset from the target.
             Vector3 targetCamPos = target.position + offset;
 
