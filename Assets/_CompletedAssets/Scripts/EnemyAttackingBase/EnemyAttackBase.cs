cdd5586e503d7522dd90f72b6a96f3efad474779
Howard <linnyflow@gmail.com>
2016-04-27 19:42:50 -0700

    Now, zombears will successfully follow the player if he is close enough (default will make them search for the base). The hellaphant also does slightly more damage now.

--- Assets/_CompletedAssets/Scripts/EnemyAttackingBase/EnemyAttackBase.cs
+++ Assets/_CompletedAssets/Scripts/EnemyAttackingBase/EnemyAttackBase.cs
@@ -9,17 +9,21 @@ public class EnemyAttackBase : MonoBehaviour
 
 
     Animator anim;                              // Reference to the animator component.
-    GameObject homeBase;                          // Reference to the player GameObject.
-    PlayerHome homeBaseHealth;                  // Reference to the player's health.
+    GameObject homeBase;                          // Reference to the homebase GameObject.
+	PlayerHome homeBaseHealth;                  // Reference to the homebase's health.
+	GameObject player;                          // Reference to the player GameObject.
+	PlayerHealth playerHealth;                  // Reference to the player's health.
     EnemyHealth enemyHealth;                    // Reference to this enemy's health.
-    bool playerInRange;                         // Whether player is within the trigger collider and can be attacked.
+    bool InRange;                         // Whether player is within the trigger collider and can be attacked.
     float timer;                                // Timer for counting up to the next attack.
 
 
     void Awake ()
     {
         // Setting up the references.
-        homeBase = GameObject.FindGameObjectWithTag ("Base");
+		homeBase = GameObject.FindGameObjectWithTag ("Base");
+		player = GameObject.FindGameObjectWithTag ("Player");
+		playerHealth = player.GetComponent <PlayerHealth> ();
         homeBaseHealth = homeBase.GetComponent <PlayerHome> ();
         enemyHealth = GetComponent<EnemyHealth>();
         anim = GetComponent <Animator> ();
@@ -28,22 +32,22 @@ public class EnemyAttackBase : MonoBehaviour
 
     void OnTriggerEnter (Collider other)
     {
-        // If the entering collider is the player...
-		if(other.gameObject == homeBase)
+        // If the entering collider is the base...
+		if(other.gameObject == homeBase || other.gameObject == player)
         {
-            // ... the player is in range.
-            playerInRange = true;
+            // ... the base is in range.
+            InRange = true;
         }
     }
 
 
     void OnTriggerExit (Collider other)
     {
-        // If the exiting collider is the player...
-		if(other.gameObject == homeBase)
+        // If the exiting collider is the base...
+		if(other.gameObject == homeBase || other.gameObject == player)
         {
-            // ... the player is no longer in range.
-            playerInRange = false;
+            // ... the base is no longer in range.
+            InRange = false;
         }
     }
 
@@ -54,14 +58,14 @@ public class EnemyAttackBase : MonoBehaviour
         timer += Time.deltaTime;
 
         // If the timer exceeds the time between attacks, the player is in range and this enemy is alive...
-        if(timer >= timeBetweenAttacks && playerInRange && enemyHealth.currentHealth > 0)
+        if(timer >= timeBetweenAttacks && InRange && enemyHealth.currentHealth > 0)
         {
             // ... attack.
             Attack ();
         }
 
-        // If the player has zero or less health...
-		if(homeBaseHealth.currentHomeHealth <= 0)
+        // If the base has zero or less health...
+		if(homeBaseHealth.currentHomeHealth <= 0 || playerHealth.currentHealth <= 0)
         {
             // ... tell the animator the player is dead.
             anim.SetTrigger ("PlayerDead");
@@ -74,11 +78,17 @@ public class EnemyAttackBase : MonoBehaviour
         // Reset the timer.
         timer = 0f;
 
-        // If the player has health to lose...
+        // If the base has health to lose...
         if(homeBaseHealth.currentHomeHealth > 0)
         {
-            // ... damage the player.
+            // ... damage the base.
 			homeBaseHealth.DamageHome(attackDamage);
         }
+		// If the player has health to lose...
+		else if(playerHealth.currentHealth > 0)
+		{
+			// ... damage the player.
+			playerHealth.TakeDamage(attackDamage);
+		}
     }
 }
