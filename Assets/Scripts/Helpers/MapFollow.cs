b3617d9dfb43e0d52a37d4d7560bb6db62864cd2
edwardszczepanski <eszczepa@uoregon.edu>
2016-05-09 18:17:57 -0700

    Added fully functioning minimap

---
+++ Assets/Scripts/Helpers/MapFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapFollow : MonoBehaviour {
+	bool onPlayer;
+	public GameObject player;
+	GameObject camera;
+
+	void Start () {
+		onPlayer = true;
+		//camera = GetComponent <Camera>();
+	}
+
+	void Update () {
+		transform.position = player.transform.position;
+		transform.position = player.transform.position;
+	}
+	
+}
