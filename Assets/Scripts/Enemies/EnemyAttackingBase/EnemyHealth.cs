b2ba4591bd80243a333ecdc120a07456e5ad378e
Alexander Owen <owen.alex47@yahoo.com>
2016-05-12 15:22:22 -0700

    fixing changes from enemies, update scene and prefab

--- Assets/Scripts/Enemies/EnemyAttackingBase/EnemyHealth.cs
+++ Assets/Scripts/Enemies/EnemyAttackingBase/EnemyHealth.cs
@@ -4,119 +4,164 @@ using System.Collections;
 
 public class EnemyHealth : MonoBehaviour
 {
-    public int startingHealth = 100;            // The amount of health the enemy starts the game with.
-    public int currentHealth;                   // The current health the enemy has.
-    public float sinkSpeed = 2.5f;              // The speed at which the enemy sinks through the floor when dead.
-    public int scoreValue = 10;                 // The amount added to the player's score when the enemy dies.
-    public AudioClip deathClip;                 // The sound to play when the enemy dies.
+	public int startingHealth = 100;            // The amount of health the enemy starts the game with.
+	public int currentHealth;                   // The current health the enemy has.
+	public float sinkSpeed = 2.5f;              // The speed at which the enemy sinks through the floor when dead.
+	public int scoreValue = 10;                 // The amount added to the player's score when the enemy dies.
+	public AudioClip deathClip;                 // The sound to play when the enemy dies.
+	public GameObject redScroll;
+	public GameObject blueScroll;
+	public bool isZapped;
 
 
-    Animator anim;                              // Reference to the animator.
-    AudioSource enemyAudio;                     // Reference to the audio source.
-    ParticleSystem hitParticles;                // Reference to the particle system that plays when the enemy is damaged.
-    CapsuleCollider capsuleCollider;            // Reference to the capsule collider.
-    bool isDead;                                // Whether the enemy is dead.
-    bool isSinking;                             // Whether the enemy has started sinking through the floor.
+	Animator anim;                              // Reference to the animator.
+	AudioSource enemyAudio;                     // Reference to the audio source.
+	ParticleSystem hitParticles;                // Reference to the particle system that plays when the enemy is damaged.
+	CapsuleCollider capsuleCollider;            // Reference to the capsule collider.
+	bool isDead;                                // Whether the enemy is dead.
+	bool isSinking;                             // Whether the enemy has started sinking through the floor.
 
 
-    void Awake ()
-    {
-        // Setting up the references.
-        anim = GetComponent <Animator> ();
-        enemyAudio = GetComponent <AudioSource> ();
-        hitParticles = GetComponentInChildren <ParticleSystem> ();
-        capsuleCollider = GetComponent <CapsuleCollider> ();
+	void Awake ()
+	{
+		// Setting up the references.
+		anim = GetComponent <Animator> ();
+		enemyAudio = GetComponent <AudioSource> ();
+		hitParticles = GetComponentInChildren <ParticleSystem> ();
+		capsuleCollider = GetComponent <CapsuleCollider> ();
 
-        // Setting the current health when the enemy first spawns.
-        currentHealth = startingHealth;
-    }
-
-
-    void Update ()
-    {
-        // If the enemy should be sinking...
-        if(isSinking)
-        {
-            // ... move the enemy down by the sinkSpeed per second.
-            transform.Translate (-Vector3.up * sinkSpeed * Time.deltaTime);
-        }
-    }
+		// Setting the current health when the enemy first spawns.
+		currentHealth = startingHealth;
+	}
 
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("TurretFire"))
-        {
-            TakeDamage(other.gameObject.GetComponent<TurretDamager>().damagePerHit, other.transform.position);
-            if (isDead)
-            {
+	void Update ()
+	{
+		// If the enemy should be sinking...
+		if(isSinking)
+		{
+			// ... move the enemy down by the sinkSpeed per second.
+			transform.Translate (-Vector3.up * sinkSpeed * Time.deltaTime);
+		}
+		if (isZapped)
+		{
+			TakeLightningDamage(5, 1, transform.position);
+		}
+	}
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("TurretFire"))
+		{
+			TakeDamage(other.gameObject.GetComponent<TurretDamager>().damagePerHit, other.transform.position);
+			if (isDead)
+			{
 				if (other.gameObject.GetComponentInParent<TurretShooter>() != null)
-                	other.gameObject.GetComponentInParent<TurretShooter>().enemyInRange = false;
-            }
-        }
-    }
+					other.gameObject.GetComponentInParent<TurretShooter>().enemyInRange = false;
+			}
+		}
+	}
 
-    public void TakeDamage(int amount, Vector3 hitPoint)
-    {
-        // If the enemy is dead...
-        if (isDead)
-            // ... no need to take damage so exit the function.
-            return;
+	public void TakeLightningDamage(int amount, double fireRate, Vector3 pos)
+	{
+		float timer = 0;
 
-        // Play the hurt sound effect.
-        enemyAudio.Play ();
+		if (isDead)
+		{
+			isZapped = false;
+			return;
+		}
+		enemyAudio.Play();
 
+		if(Time.time > timer + fireRate)
+		{
+			currentHealth -= amount;
+			hitParticles.transform.position = pos;
+			hitParticles.Play();
+			timer = Time.time;
+		}
 
-        // Reduce the current health by the amount of damage sustained.
-        currentHealth -= amount;
-        
-        // Set the position of the particle system to where the hit was sustained.
-        hitParticles.transform.position = hitPoint;
+		if(currentHealth <= 0)
+		{
+			Death();
+			isZapped = false;
+		}
+	}
 
-        // And play the particles.
-        hitParticles.Play();
+	public void TakeDamage(int amount, Vector3 hitPoint)
+	{
+		// If the enemy is dead...
+		if (isDead)
+			// ... no need to take damage so exit the function.
+			return;
 
-        // If the current health is less than or equal to zero...
-        if(currentHealth <= 0)
-        {
-            // ... the enemy is dead.
-            Death ();
-        }
-    }
-
-
-    void Death ()
-    {
-        // The enemy is dead.
-        isDead = true;
-
-        // Turn the collider into a trigger so shots can pass through it.
-        capsuleCollider.isTrigger = true;
-
-        // Tell the animator that the enemy is dead.
-        anim.SetTrigger ("Dead");
-
-        // Change the audio clip of the audio source to the death clip and play it (this will stop the hurt clip playing).
-        enemyAudio.clip = deathClip;
-        enemyAudio.Play ();
-    }
+		// Play the hurt sound effect.
+		enemyAudio.Play ();
 
 
-    public void StartSinking ()
-    {
-        // Find and disable the Nav Mesh Agent.
-        GetComponent <NavMeshAgent> ().enabled = false;
+		// Reduce the current health by the amount of damage sustained.
+		currentHealth -= amount;
 
-        // Find the rigidbody component and make it kinematic (since we use Translate to sink the enemy).
-        GetComponent <Rigidbody> ().isKinematic = true;
+		// Set the position of the particle system to where the hit was sustained.
+		hitParticles.transform.position = hitPoint;
 
-        // The enemy should no sink.
-        isSinking = true;
+		// And play the particles.
+		hitParticles.Play();
 
-        // Increase the score by the enemy's score value.
-        ScoreManager.score += scoreValue;
+		// If the current health is less than or equal to zero...
+		if(currentHealth <= 0)
+		{
+			// ... the enemy is dead.
+			Death ();
+		}
+	}
 
-        // After 2 seconds destory the enemy.
-        Destroy (gameObject, 0.3f);
-    }
+
+	void Death ()
+	{
+		// The enemy is dead.
+		isDead = true;
+
+		// Turn the collider into a trigger so shots can pass through it.
+		capsuleCollider.isTrigger = true;
+
+		// Tell the animator that the enemy is dead.
+		anim.SetTrigger ("Dead");
+
+		// Change the audio clip of the audio source to the death clip and play it (this will stop the hurt clip playing).
+		enemyAudio.clip = deathClip;
+		enemyAudio.Play ();
+		if (this.transform.name.StartsWith("Hell")) {
+			Vector3 deathPos = this.transform.position;
+			deathPos.y += 1;
+			if (Random.value > 0) {
+				if (Random.value < .5) {
+					Instantiate (redScroll, deathPos, Quaternion.identity);
+				} else {
+					Instantiate (blueScroll, deathPos, Quaternion.identity);
+				}
+			}
+		}
+
+	}
+
+
+	public void StartSinking ()
+	{
+		// Find and disable the Nav Mesh Agent.
+		GetComponent <NavMeshAgent> ().enabled = false;
+
+		// Find the rigidbody component and make it kinematic (since we use Translate to sink the enemy).
+		GetComponent <Rigidbody> ().isKinematic = true;
+
+		// The enemy should no sink.
+		isSinking = true;
+
+		// Increase the score by the enemy's score value.
+		ScoreManager.score += scoreValue;
+
+		// After 2 seconds destory the enemy.
+		Destroy (gameObject, 0.3f);
+	}
 }
