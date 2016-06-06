34e750df9b4d1e48d287416a8bfc638a49d7c6ef
Brennan <jeremb@gmail.com>
2016-06-05 19:30:57 -0700

    changed gun bullets

--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -24,6 +24,7 @@ public class PlayerShooting : MonoBehaviour
 	public Toggle Shotguntoggle;
 	public Toggle Lasertoggle;
 	public Text LaserCDtext;
+	public Material GunMaterial;
 
     RaycastHit shootHit;                            // A raycast hit to get information about what was hit.
     int shootableMask;                              // A layer mask so the raycast only hits things on the shootable layer.
@@ -75,7 +76,7 @@ public class PlayerShooting : MonoBehaviour
 		gunShotClips[2].volume = 0.8f;
 
 		gunList [0] = new GunClass (20, 0.15f, 32f, gunShotClips[0], 0.8f); // Rifle
-		gunList [1] = new GunClass (80, 0.8f, 15f, gunShotClips[2], 0.2f); // Shotgun
+		gunList [1] = new GunClass (80, 0.8f, 10f, gunShotClips[2], 0.2f); // Shotgun
 		gunList [2] = new GunClass (200, 1.5f, 50f, gunShotClips[1], 1.1f); // Laser
 		gunType = Gun.Rifle;
     }
@@ -84,7 +85,8 @@ public class PlayerShooting : MonoBehaviour
     {
         // Add the time since Update was last called to the timer.
         timer += Time.deltaTime;
-		laserCD += Time.deltaTime;
+		//laserCD += Time.deltaTime;
+		laserCD = 16;
 		interval += Time.deltaTime;
 		if (laserCD < 15f){
 			if (interval > 1f) {
@@ -205,6 +207,7 @@ public class PlayerShooting : MonoBehaviour
 	        // Enable the line renderer and set it's first position to be the end of the gun.
 	        gunLine.enabled = true;
 	        gunLine.SetPosition(0, transform.position);
+			GunMaterial.mainTextureScale = new Vector2 (20, 1);
 
 	        // Set the shootRay so that it starts at the end of the gun and points forward from the barrel.
 
@@ -214,11 +217,6 @@ public class PlayerShooting : MonoBehaviour
 
 
 			if (gunType == Gun.Rifle) {
-				faceLight.color = Color.yellow;
-				gunLine.material.color = Color.yellow;
-				gunLine.SetColors (Color.yellow, Color.yellow);
-				gunparticle.startColor = Color.yellow;
-				gunLight.color = Color.yellow;
 				gunLine.SetWidth (0.05f, 0.05f);
 				shootRay.direction = transform.forward;
 				ifHit (shootRay, currentRange, currentDamage, gunLine);
@@ -226,14 +224,7 @@ public class PlayerShooting : MonoBehaviour
 
 
 			if (gunType == Gun.Shotgun) {
-				faceLight.color = Color.red;
-				gunLine.material.color = Color.red;
-				leftline.material.color = Color.red;
-				rightline.material.color = Color.red;
-				gunparticle.startColor = Color.red;
-				gunLine.SetColors (Color.red, Color.red);
 				gunLine.SetWidth (0.05f, 0.05f);
-				gunLight.color = Color.red;
 				leftline.enabled = true;
 				leftline.SetPosition (0, transform.position);
 				rightline.enabled = true;
@@ -251,12 +242,8 @@ public class PlayerShooting : MonoBehaviour
 			if (gunType == Gun.Laser) {
 				// TODO activate UI element to display cooldown on OP laser
 				laserCD = 0f;
-				faceLight.color = Color.blue;
-				gunLine.material.color = Color.blue;
-				gunLine.SetColors (Color.blue, Color.blue);
-				gunparticle.startColor = Color.blue;
-				gunLight.color = Color.blue;
-				gunLine.SetWidth (0.05f, 1f);
+				gunLine.SetWidth (3f, 3f);
+				GunMaterial.mainTextureScale = new Vector2 (2f, 1f);
 				shootRay.direction = transform.forward;
 
 				RaycastHit[] rayhits;
